d0b06a5034dde90443c3cd38e25aefb9b8134212
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-03-18 11:18:23 +0100

    Avancement du contacts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// Initialize and add the map
+// MAPS
 function initMap() {
     const campus = { lat: 46.6687245, lng: 5.5536708 };
     const map = new google.maps.Map(document.getElementById("map"), {
